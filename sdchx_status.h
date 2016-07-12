78707407812ffefb0e75960c390ab9600d506a1b
Vasily Chekalkin <bacek@yandex-team.ru>
2016-07-12 22:12:43 +1000

    Add status

---
+++ sdchx_status.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2016 Yandex LLC. All rights reserved.
+// Author: Vasily Chekalkin <bacek@yandex-team.ru>
+
+#ifndef SDCHX_STATUS_H_
+#define SDCHX_STATUS_H_
+
+namespace sdchx {
+
+enum Status {
+  STATUS_OK,       // everything is ok
+  STATUS_ERROR,    // some bad things happened
+};
+
+
+}  // namespace sdchx
+
+#endif  // SDCHX_STATUS_H_
+

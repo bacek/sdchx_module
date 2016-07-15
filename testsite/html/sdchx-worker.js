0a728b2c85cc4718b8a8c7cf3d25991b21a3d08a
Vasily Chekalkin <bacek@yandex-team.ru>
2016-07-15 18:38:40 +1000

    Cleanups

--- testsite/html/sdchx-worker.js
+++ testsite/html/sdchx-worker.js
@@ -1,78 +1,96 @@
-// [Working example](/serviceworker-cookbook/json-cache/).
+const CACHE_NAME = "sdchx-cache";
+// const DICTIONARIES_CACHE_NAME = "sdchx-dictionaries";
 
-var CACHE_NAME = 'sdchx-cache';
-var DICTIONARIES_CACHE_NAME = 'sdchx-dictionaries';
-
+/**
+ * Process response from network
+ */
 function processResponse(response) {
-  console.log('Processing response', response);
+  console.log("Processing response", response);
   extractDictionaryLinks(response);
   return response;
 }
-
+/**
+ * Extract Link headers from response and fetch advertised dictionaries
+ */
 function extractDictionaryLinks(response) {
-  console.log('extractDictionaryLinks');
-  var links = response.headers
-    .getAll('Link');
-  console.log('Links', links);
-  var urls = links.map(v => {
-    var m = v.match( /<(\S+)>.*rel="sdchx-dictionary"/ );
-    console.log('Match', m);
+  console.log("extractDictionaryLinks");
+  let links = response.headers
+    .getAll("Link");
+  console.log("Links", links);
+  let urls = links.map(v => {
+    let m = v.match(/<(\S+)>.*rel="sdchx-dictionary"/);
+    console.log("Match", m);
     return m[1];
   });
-  console.log('urls', urls);
-  urls = urls.filter(v => { return v != null});
-  // var urls = links.map(v => { v.match(/<(\S+)>.*rel="sdchx-dictionary"/)[1] }).filter(v => { v != null});
-  console.log('final', urls);
+  console.log("urls", urls);
+  urls = urls.filter(v => {
+    return v !== null;
+  });
+  // let urls = links.map(v => { v.match(/<(\S+)>.*rel="sdchx-dictionary"/)[1] }).filter(v => { v != null});
+  console.log("final", urls);
   urls.forEach(doFetch);
 }
-
+/**
+ * Decode VCDiff encoded content.
+ * Browser will unzip it for us.
+ */
 function maybeDecodeContent(response) {
-  console.log('maybeDecodeContent', response);
+  console.log("maybeDecodeContent", response);
   return response;
 }
-
+/**
+ * Cache result from network request
+ */
 function cacheIt(request, response) {
-  // console.log('cacheIt request', request);
-  // console.log('cacheIt response', response);
+  // console.log("cacheIt request", request);
+  // console.log("cacheIt response", response);
   caches.open(CACHE_NAME).then(cache => {
-    console.log('caching');
+    console.log("caching");
     cache.put(request, response.clone());
   });
   return response;
 }
-
+/**
+ * Actually fetch url.
+ */
 function doFetch(url) {
-  console.log('Fetching ', url);
-  return fetch(createRequest(url)).then(processResponse).then(maybeDecodeContent);
+  console.log("Fetching ", url);
+  return fetch(createRequest(url))
+          .then(cacheIt)
+          .then(processResponse)
+          .then(maybeDecodeContent);
 }
 
+/**
+ * Create SDCHx-enabled GET request for given url
+ */
 function createRequest(url) {
-  var req = new Request(url, {
+  let req = new Request(url, {
     headers: {
-      'X-Accept-Encoding': 'sdchx'
+      "X-Accept-Encoding": "sdchx"
     }
   });
-  for (var h of req.headers) {
-    console.log('Header:', h);
+  for (let h of req.headers) {
+    console.log("Header:", h);
   }
   return req;
 }
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", event => {
   // Message to simply show the lifecycle flow
-  console.log('[install] Kicking off service worker registration!');
+  console.log("[install] Kicking off service worker registration!");
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-  if (event.request.method === 'GET') {
-    console.log('Handling ', event.request.url);
+self.addEventListener("fetch", event => {
+  if (event.request.method === "GET") {
+    console.log("Handling ", event.request.url);
     event.respondWith(doFetch(event.request.url));
   }
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", event => {
   // Claim the service work for this client, forcing `controllerchange` event
-  console.log('[activate] Claiming this service worker!');
+  console.log("[activate] Claiming this service worker!");
   event.waitUntil(self.clients.claim());
 });

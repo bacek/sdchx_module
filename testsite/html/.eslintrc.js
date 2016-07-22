a49bf3b64c4469ef64024c884fafd4a75421b3fa
Vasily Chekalkin <bacek@yandex-team.ru>
2016-07-22 14:22:08 +1000

    eslint config

---
+++ testsite/html/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "google",
+    "installedESLint": true,
+    "env" : {
+      "browser": true,
+      "worker": true,
+      "es6": true,
+    },
+    "rules": {
+      "valid-jsdoc": "off",
+      "comma-dangle": "off",
+    }
+};
